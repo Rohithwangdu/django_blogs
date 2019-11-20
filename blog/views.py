4b73d38ddb50c626cf3d6be5732ac98d9ee813e3
rwang <rohith90reddy@gmail.com>
2019-11-20 03:25:56 -0800

    created blog post model and sample blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts =[
-    {
-        'author': 'Iron man',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted':'Nov 18, 2019'
-    },
-    {
-        'author': 'Capt Marvel',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted':'Nov 19, 2019'
-    }
-]
 def home(request):
     '''A sample home page'''
     context = {
-        'posts':posts
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
     '''A sample about page'''
-    return render(request, 'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
