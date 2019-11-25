bdbb524c18e54fa973ea0b3052eb85122ad23e1a
rwang <rohith90reddy@gmail.com>
2019-11-24 20:25:39 -0800

    Added user profile page

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
+    """ User registration page"""
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -14,3 +16,9 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    """Displays user profile"""
+    return render(request, 'users/profile.html')
